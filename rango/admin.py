11804e2309d7f3626176ba158b94aed2139a78c7
Jianhua You <2534534y@student.gla.ac.uk>
2021-02-04 14:56:11 +0000

    third

--- rango/admin.py
+++ rango/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
 from rango.models import Category, Page
+# Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('id','title','category','url')
+
+class CategoryAdmin(admin.ModelAdmin):
+    lisst_display = ('views','likes')
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Page,PageAdmin)
